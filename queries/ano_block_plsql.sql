3d320554249044c7df2dd88c0b578c8582d0e954
joydip mondal <joydip.mondal@gmail.com>
2023-07-10 10:32:58 +0530

    pl-sql introduction

---
+++ queries/ano_block_plsql.sql
@@ -0,0 +1,13 @@
+SET SERVEROUTPUT ON;
+DECLARE
+    message VARCHAR2(25) := 'welcome to pl-sql';
+    div_result number;
+    first_val NUMBER := 10;
+    second_val NUMBER := 0;
+BEGIN
+    DBMS_OUTPUT.put_line(message);
+    div_result := first_val/second_val;
+    EXCEPTION
+        WHEN ZERO_DIVIDE THEN
+            DBMS_OUTPUT.put_line(SQLERRM);
+END;
